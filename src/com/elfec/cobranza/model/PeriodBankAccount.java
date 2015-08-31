4064939c3754e4dd0803ea4fc7106997bfc81ebd
diegoRodriguezAguila <diroag@gmail.com>
2015-08-31 09:01:37 -0400

    Modificado posible error de fechas de periodo de caja

--- src/com/elfec/cobranza/model/PeriodBankAccount.java
+++ src/com/elfec/cobranza/model/PeriodBankAccount.java
@@ -10,6 +10,7 @@ import com.elfec.cobranza.model.serializers.JodaDateTimeSerializer;
 
 /**
  * Almacena los BAN_CTAS_PER
+ * 
  * @author drodriguez
  *
  */
@@ -18,23 +19,23 @@ public class PeriodBankAccount extends Model {
 	/**
 	 * IDBANCO en Oracle
 	 */
-	@Column(name = "BankId", notNull=true)
-	private int bankId;	
+	@Column(name = "BankId", notNull = true)
+	private int bankId;
 	/**
 	 * IDBAN_CTA en Oracle
 	 */
-	@Column(name = "BankAccountId", notNull=true, index=true)
-	private int bankAccountId;	
+	@Column(name = "BankAccountId", notNull = true, index = true)
+	private int bankAccountId;
 	/**
 	 * NROPERIODO en Oracle
 	 */
-	@Column(name = "PeriodNumber", notNull=true, index=true)
-	private int periodNumber;	
+	@Column(name = "PeriodNumber", notNull = true, index = true)
+	private int periodNumber;
 	/**
 	 * IDCAJERO en Oracle
 	 */
 	@Column(name = "CashierId")
-	private int cashierId;	
+	private int cashierId;
 	/**
 	 * FECHA en Oracle
 	 */
@@ -54,7 +55,7 @@ public class PeriodBankAccount extends Model {
 	 * IDSTATUS en Oracle
 	 */
 	@Column(name = "StatusId")
-	private short statusId;	
+	private short statusId;
 	/**
 	 * IDZONA en Oracle
 	 */
@@ -65,12 +66,10 @@ public class PeriodBankAccount extends Model {
 	 */
 	@Column(name = "EnterpriseId")
 	private int enterpriseId;
-	
+
 	public PeriodBankAccount() {
 		super();
 	}
-	
-	
 
 	public PeriodBankAccount(int bankId, int bankAccountId, int periodNumber,
 			int cashierId, DateTime periodDate, DateTime openingTime,
@@ -89,46 +88,55 @@ public class PeriodBankAccount extends Model {
 		this.enterpriseId = enterpriseId;
 	}
 
-
-
 	/**
 	 * Obtiene el periodo de la cuenta
+	 * 
 	 * @param cashdeskNumber
 	 * @return
 	 */
-	public static PeriodBankAccount findByCashdeskNumberAndDate(int cashdeskNumber)
-	{
-		return new Select().from(PeriodBankAccount.class)
-				.where("BankAccountId = ? AND PeriodDate = ?", cashdeskNumber, 
-				(new JodaDateTimeSerializer()).serialize(DateTime.now().withTimeAtStartOfDay())).executeSingle();
+	public static PeriodBankAccount findByCashdeskNumberAndDate(
+			int cashdeskNumber) {
+		return new Select()
+				.from(PeriodBankAccount.class)
+				.where("BankAccountId = ? AND PeriodDate = ?",
+						cashdeskNumber,
+						(new JodaDateTimeSerializer()).serialize(DateTime.now()
+								.withTime(0, 0, 0, 0))).executeSingle();
 	}
-	
-	//#region Getters y Setters
+
+	// #region Getters y Setters
 	public int getBankId() {
 		return bankId;
 	}
+
 	public void setBankId(int bankId) {
 		this.bankId = bankId;
 	}
+
 	public int getBankAccountId() {
 		return bankAccountId;
 	}
+
 	public void setBankAccountId(int bankAccountId) {
 		this.bankAccountId = bankAccountId;
 	}
+
 	public int getPeriodNumber() {
 		return periodNumber;
 	}
+
 	public void setPeriodNumber(int periodNumber) {
 		this.periodNumber = periodNumber;
 	}
+
 	public int getCashierId() {
 		return cashierId;
 	}
+
 	public void setCashierId(int cashierId) {
 		this.cashierId = cashierId;
 	}
-	
+
 	public DateTime getPeriodDate() {
 		return periodDate;
 	}
@@ -145,31 +153,37 @@ public class PeriodBankAccount extends Model {
 		this.openingTime = openingTime;
 	}
 
-	
 	public DateTime getClosingDateTime() {
 		return closingDateTime;
 	}
+
 	public void setClosingDateTime(DateTime closingDateTime) {
 		this.closingDateTime = closingDateTime;
 	}
+
 	public short getStatusId() {
 		return statusId;
 	}
+
 	public void setStatusId(short statusId) {
 		this.statusId = statusId;
 	}
+
 	public int getZoneId() {
 		return zoneId;
 	}
+
 	public void setZoneId(int zoneId) {
 		this.zoneId = zoneId;
 	}
+
 	public int getEnterpriseId() {
 		return enterpriseId;
 	}
+
 	public void setEnterpriseId(int enterpriseId) {
 		this.enterpriseId = enterpriseId;
 	}
-	
-	//#endregion
+
+	// #endregion
 }
