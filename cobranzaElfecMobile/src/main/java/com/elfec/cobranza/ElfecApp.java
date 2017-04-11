1d9b52a1962409fb5d670743250aa740237d5005
Diego Rodriguez Aguila <diroag@gmail.com>
2017-04-11 17:08:08 -0400

    Added calligraphy and joda initializations

--- cobranzaElfecMobile/src/main/java/com/elfec/cobranza/ElfecApp.java
+++ cobranzaElfecMobile/src/main/java/com/elfec/cobranza/ElfecApp.java
@@ -3,12 +3,19 @@ package com.elfec.cobranza;
 import com.activeandroid.app.Application;
 import com.elfec.cobranza.settings.PreferencesManager;
 
+import net.danlew.android.joda.JodaTimeAndroid;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 public class ElfecApp extends Application {
 
-	@Override
-	public void onCreate()
-	{
-		super.onCreate();
-		PreferencesManager.initialize(this);
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/segoe_ui.ttf")
+                .setFontAttrId(R.attr.fontPath).build());
+        JodaTimeAndroid.init(this);
+        PreferencesManager.initialize(this);
+    }
 }
